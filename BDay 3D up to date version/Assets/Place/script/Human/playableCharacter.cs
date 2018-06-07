8ff179808662337dd7f7431585f83bedf1d39688
Eidolon Seven <zpmao77@gmail.com>
2018-06-07 10:29:31 -0400

    Apparently the previous changes never happened
    Same as prev

--- BDay 3D up to date version/Assets/Place/script/Human/playableCharacter.cs
+++ BDay 3D up to date version/Assets/Place/script/Human/playableCharacter.cs
@@ -5,59 +5,192 @@ namespace BlackFriday
 {
     public class PlayableCharacter : MonoBehaviour
     {
-
+        //Basic Characteristics
         protected string characterName;//Name of the person, set during the constructor and nopt changed after
+
+        //HP and manipulation methodes
         protected int healthPoint;//Abreviated to HP when refering in documentation
+        protected int maxHealthPoint; //Maximum Health point possible
+        protected void summationHealth(int deltaHP); //adds or subtracts health by a flat number
+        protected void percentHPChangeIncreaseMax(int deltaHpPercentUpMax); //Percent adjustment of HP increase (Max health)
+        protected void percentHPChangeDecreaseMax(int deltaHpPercentDownMax); //Percent adjustment of HP increase (Max Health)
+        protected void percentHPChangeIncreaseCur(int deltaHpPercentUpCur); //Percent adjustment of HP increase (Current health)
+        protected void percentHPChangeDecreaseCur(int deltaHpPercentDownCur); //Percent adjustment of HP increase (Current Health)
+        
+        //Score related functions
         protected int cashStore;//Current availible cash for purchasing
         protected int goalScore;//game victory condition. (Perhaps this should be part of the specific map as opposed to player?)
+
+        //Mobility modifiers
         protected float movementSpeed;//Player movement speed (some Unit-per-tick)
+        protected void summationMS(int deltaMS); //adds or subtracts MS by a flat number
+        protected void percentMSChangeIncreaseCur(int deltaMSPercentUpCur); //Percent adjustment of MS increase (Current MS)
+        protected void percentMSChangeDecreaseCur(int deltaMSPercentDownCur); //Percent adjustment of MS increase (Current MS)
+
+        //Player condition/Status 
         protected bool isDead;//If the player is dead or not
-        protected int playertatus;//enumerated status
-                            // 0 = Despawned
-                            // 1 = Normal
-                            // 2 = Prone
-                            // 3 = Stunned
-                            // ...
+        protected int playerStatus;//enumerated status
+                                   // 0 = Despawned
+                                   // 1 = Normal
+                                   // 2 = Prone
+                                   // 3 = Stunned
+                                   // 4 = Dead
+                                   // 5 = Jumping...
 
-        protected CapsuleCollider bodySpace;
-        protected SphereCollider vulnerabilitySpace;
-
-        protected List<Merch> shoppoingCart;
-        protected int cartSize;
-        protected bool cartFull;
-
-        protected List<Consumable> inventory;
-        protected int invSize;
-        protected bool invFull;
+        //Jumping: Normally players would be blocked by certain objects and obstacles (collision on). "Jump" just toggles collision on for a few seconds.
+        //(We can also actually make the jumper class where they can jump longer or something)         
+       
+        //These are the rigid bodyies for the player
+        protected CapsuleCollider bodySpace; //The physical body of the player which will be textured
+        protected SphereCollider vulnerabilitySpace; //this is basically the hitbox of the player
         
+        //Gameplay elements 
+        protected List<Merch> shoppoingCart; //Current list of merch items collected 
+        protected bool addMerchToCart(Merch addItem);//Mutator to add an item
+        protected int cartSize; //Maximun number of items that can be placed within the cart
+        protected int cartCounter; //Current max cart item slot used
+        protected bool cartFull; //When the cart is full you cannot add any more (0 = not full, 1 = full)
+
+        protected List<Consumable> inventory; //Inventory for consumable non-merch items, Current active item is in slot 0
+        protected int invSize; //Maximum number of items that can be placed within the inventory
+        protected bool invFull; //When the inventory is full you cannot add any more (0 = not full, 1 = full) 
 
 
 
-        protected float HealthPoint
+        /// <summary>
+        /// HP Related Methodes Implementation
+        /// </summary>
+
+        //Overwriting the defult get and set functions to simultaniously update ifDead if dead
+        protected int healthPoint
         {
+            // get remains the same
             get
             {
                 return healthPoint;
             }
+
+            // Set automatically checks if if the person is dead
             set
             {
-                if (value < 0) {
+                if (value <= 0) {
                     healthPoint = 0;
-                    isDead = true;//move this to update 
-                    //also we will dael with dropping of the loots
+                    isDead = true;
+                    playerStatus = 4;
+                    //move this to update 
+                    //also we will deal with dropping of the loots
                 }
+
+                if (value >= maxHealthPoint)
+                {
+                    healthPoint = maxHealthPoint;
+                }
+
+                if(isDead == true && value > 0)
+                {
+                    healthPoint = value;
+                    isDead = false;
+                    playerStatus = 2;
+                }
+
             }
         }
 
-        protected int CartSize
+        //adds or subtracts health by a flat number
+        protected void summationHealth(int deltaHP)
+        {
+            int temp = healthPoint;
+            temp = temp + deltaHP;
+            healthPoint = temp;
+        }
+
+        //Percent adjustment of HP increase (Max health)
+        protected void percentHPChangeIncreaseMax(int deltaHpPercentUpMax)
+        {
+            int temp = healthPoint;
+            temp = temp + maxHealthPoint * (1 + deltaHpPercentUpMax);
+            healthPoint = temp;
+        }
+
+        //Percent adjustment of HP increase (Max Health)
+        protected void percentHPChangeDecreaseMax(int deltaHpPercentDownMax)
+        {
+            int temp = healthPoint;
+            temp = temp - maxHealthPoint * (deltaHpPercentUpMax);
+            healthPoint = temp;
+        }
+
+        //Percent adjustment of HP increase (Current health)
+        protected void percentHPChangeIncreaseCur(int deltaHpPercentUpCur)
+        {
+            int temp = healthPoint;
+            temp = temp * (1 + deltaHpPercentUpCur);
+            healthPoint = temp;
+        }
+
+        //Percent adjustment of HP increase (Current Health)
+        protected void percentHPChangeDecreaseCur(int deltaHpPercentDownCur)
+        {
+            int temp = healthPoint;
+            temp =temp * (deltaHpPercentDownCur);
+            healthPoint = temp;
+        }
+
+
+        /// <summary>
+        /// Movespeed mutators
+        /// </summary>
+   
+
+        //adds or subtracts MS by a flat number
+        protected void summationMS(int deltaMS)
+        {
+           float temp = movementSpeed;
+            temp = temp + deltaMS;
+            movementSpeed = deltaMS;
+        }
+
+        //Percent adjustment of MS increase (Current MS)
+        protected void percentMSChangeIncreaseCur(int deltaMSPercentUpCur)
+        {
+            int temp = movementSpeed;
+            temp = temp * (1 + deltaMSPercentUpCur);
+            movementSpeed = temp;
+        }
+
+        //Percent adjustment of MS increase (Current MS)
+        protected void percentMSChangeDecreaseCur(int deltaMSPercentDownCur)
+        {
+            int temp = movementSpeed;
+            temp = temp * (1 - deltaMSPercentUpCur);
+            movementSpeed = temp;
+        }
+        
+
+
+        /// <summary>
+        /// Cart Related Stuff
+        /// </summary>
+
+        //Update the CartCounter
+        protected int CartCounter
         {
             get
             {
-                return cartSize;
+                return CartCounter;
             }
             set
             {
-                if (value < 0) cartSize = 0;
+                if (value < 0) CartCounter = 0;
+                else if (value >= cartSize)
+                {
+                    cartFull = true;
+                    CartCounter = cartSize;
+                }
+                else if (value < cartSize)
+                {
+                    cartFull = false;
+                }
 
             }
         }
